d0c2e41940dc05b91a49b79e2243eae64a3b139a
Mostafa Mokhtar <ttmahdy19@hotmail.com>
2015-05-27 09:33:36 -0700

    Push down the group by

--- primitives/primitive_shuffle_join_union_all_with_groupby.sql
+++ primitives/primitive_shuffle_join_union_all_with_groupby.sql
@@ -3,24 +3,35 @@ select
     count(*) rowcount
 from
     (select 
-        ss_cdemo_sk, ss_store_sk,ss_item_sk, ss_ticket_number
-    from
-        store_sales a, store_returns b
-    where
-        a.ss_item_sk = b.sr_item_sk
-            and a.ss_ticket_number = b.sr_ticket_number 
-            and ss_sold_date_sk between 2450816 and 2451500
-			and sr_returned_date_sk between 2450816 and 2451500
-            union all 
-	select 
-        ss_cdemo_sk, ss_store_sk,ss_item_sk, ss_ticket_number
-    from
-        store_sales c, store_returns d
-    where
-        c.ss_item_sk = d.sr_item_sk
-            and c.ss_ticket_number = d.sr_ticket_number
-			and ss_sold_date_sk between 2450816 and 2451500
-			and sr_returned_date_sk between 2450816 and 2451500) t
-group by t.ss_cdemo_sk,t.ss_store_sk,t.ss_item_sk , t.ss_ticket_number
-having rowcount > 1000000000;
+    ss_cdemo_sk,
+    ss_store_sk,
+    ss_item_sk,
+    ss_ticket_number,
+    count(*) rowcount
+from
+    store_sales a,
+    store_returns b
+where
+    a.ss_item_sk = b.sr_item_sk
+        and a.ss_ticket_number = b.sr_ticket_number
+        and ss_sold_date_sk between 2450816 and 2451500
+        and sr_returned_date_sk between 2450816 and 2451500
+group by ss_cdemo_sk , ss_store_sk , ss_item_sk , ss_ticket_number
+having rowcount > 1000000000 
+union all select 
+    ss_cdemo_sk,
+    ss_store_sk,
+    ss_item_sk,
+    ss_ticket_number,
+    count(*) rowcount
+from
+    store_sales c,
+    store_returns d
+where
+    c.ss_item_sk = d.sr_item_sk
+        and c.ss_ticket_number = d.sr_ticket_number
+        and ss_sold_date_sk between 2450816 and 2451500
+        and sr_returned_date_sk between 2450816 and 2451500
+group by ss_cdemo_sk , ss_store_sk , ss_item_sk , ss_ticket_number
+having rowcount > 1000000000) t;
    
